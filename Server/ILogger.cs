f844ed52e00e65967c62fb73a0f4aa5e3b331fcd
Wade Carlson <wcarlsondev@gmail.com>
2018-01-25 08:01:45 -0600

    removed unused libraries

--- Server/ILogger.cs
+++ Server/ILogger.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Server
+﻿namespace Server
 {
-     public interface ILogger
+    public interface ILogger
     {
         void LogMessage(string message);
         
--- Server/Server.cs
+++ Server/Server.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 
 namespace Server
 {
-     public class Server : INotifier
+    public class Server : INotifier
     {
         private int userNumber = 1;
         User user;
